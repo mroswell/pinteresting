b4b7ad20be9910a2e331692385b071f080bc37bb
Marjorie Roswell <mroswell@gmail.com>
2014-02-10 01:22:40 -0500

    update

--- app/controllers/pins_controller.rb
+++ app/controllers/pins_controller.rb
@@ -1,7 +1,7 @@
 class PinsController < ApplicationController
   before_action :set_pin, only: [:show, :edit, :update, :destroy]
   before_action :authenticate_user!, except:  [:index, :show ]
-  before_action :correct_user, only: [:edit, :update, :destroy]
+  before_action :correct_user, only: [ :edit, :update, :destroy ]
   # GET /pins
   def index
     @pins = Pin.all
