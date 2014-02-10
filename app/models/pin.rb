e760ba8636f2cb5b39b51c9dbb36248e7bcf01b1
Marjorie Roswell <mroswell@gmail.com>
2014-02-10 02:05:51 -0500

    return to previous version of paperclip

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -2,6 +2,6 @@ class Pin < ActiveRecord::Base
 	belongs_to :user
 
 	has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>"}
-	validates_attachment_content_type :file_name, :content_type => %w(image/jpeg image/jpg image/png)
+	# validates_attachment_content_type :image, :content_type => %w(image/jpeg image/jpg image/png)
 
 end
