85a17764ce9b2dd264282d0edd89959fa53ecf07
Marjorie Roswell <mroswell@gmail.com>
2014-02-10 22:10:50 -0500

    add pagination to pins

--- Gemfile
+++ Gemfile
@@ -15,6 +15,8 @@ gem 'devise', '~>3.2.2'
 gem 'paperclip', '~>3.0'
 gem 'aws-sdk', '~> 1.33.0'
 gem 'masonry-rails', '~>0.2.0'
+gem 'will_paginate', '~> 3.0'
+gem 'will_paginate-bootstrap'
 
 
 group :development, :test do
--- app/controllers/pins_controller.rb
+++ app/controllers/pins_controller.rb
@@ -4,7 +4,7 @@ class PinsController < ApplicationController
   before_action :correct_user, only: [ :edit, :update, :destroy ]
   # GET /pins
   def index
-    @pins = Pin..order('created_at DESC')
+    @pins = Pin.order('created_at DESC').paginate(:page => params[:page], :per_page => 20)
   end
 
   # GET /pins/1
